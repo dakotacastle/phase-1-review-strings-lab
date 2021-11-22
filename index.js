e844296bc32a0f6e9dda214cbb46b689b4209229
Dakota Castle <barrettcastle@gmail.com>
2021-11-22 12:30:22 -0500

    commit tree 0775bf194e354906f485d5c39794b1013bfd3909

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+let currentUser = "Dakota"
+
+const welcomeMessage = "Welcome to Flatbook, Dakota!";
+
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, DAKOTA!";
+
+const shortGreeting = "Welcome, D!"
